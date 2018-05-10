582eb63121eba017a26c9deb0fb887cde86deda7
Simion Alex <simion.alexgabriel@yahoo.com>
2018-05-10 10:42:10 +0300

    Format code

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstring>
 #include <vector>
+
 using namespace std;
 
 // Folosit ca sa memoram o productie
@@ -14,7 +15,7 @@ struct Productie {
 struct Gramatica {
     Productie *productii;
     int nrProductii;
-    vector<string> cuvinteGenerate;
+    vector <string> cuvinteGenerate;
 };
 
 /**
@@ -29,8 +30,8 @@ void generareCuvinte(string cuvant, Gramatica &g, int limitaLitere) {
     string copieCuvant;
 
     // Verificam daca avem neterminal si salvam pozitia daca da
-    for(int i = 0; i <= cuvant.length(); i++) {
-        if(cuvant[i] >= 'A' && cuvant[i] <= 'Z') {
+    for (int i = 0; i <= cuvant.length(); i++) {
+        if (cuvant[i] >= 'A' && cuvant[i] <= 'Z') {
             literaNeterminal = cuvant[i];
             pozitieNeterminal = i;
             break;
@@ -39,23 +40,23 @@ void generareCuvinte(string cuvant, Gramatica &g, int limitaLitere) {
 
     // Daca nu exista neterminale in cuvant si daca ne incadram in limita de lungime atunci adaugam cuvantul
     // la vectorul de cuvinte generate
-    if(pozitieNeterminal == -1 && cuvant.length() <= limitaLitere){
-        if(cuvant.length() == 0) g.cuvinteGenerate.emplace_back("Cuvantul vid!");
+    if (pozitieNeterminal == -1 && cuvant.length() <= limitaLitere) {
+        if (cuvant.length() == 0) g.cuvinteGenerate.emplace_back("Cuvantul vid!");
         else g.cuvinteGenerate.emplace_back(cuvant);
         return;
     }
-    // Daca mai exista neterminale in cuvant atunci le dezvoltam
-    else if(pozitieNeterminal != -1){
-        for(int i = 0; i < g.nrProductii; i++) {
+        // Daca mai exista neterminale in cuvant atunci le dezvoltam
+    else if (pozitieNeterminal != -1) {
+        for (int i = 0; i < g.nrProductii; i++) {
             // Cautam in gramatica productia cu neterminalul curent, daca cuvantul se incadreaza in limita de litere atunci dezvoltam
-            if(g.productii[i].litera == literaNeterminal && cuvant.length() <= limitaLitere) {
+            if (g.productii[i].litera == literaNeterminal && cuvant.length() <= limitaLitere) {
                 // Facem o copie a cuvantul pt a o folosi in recursivitate(vrem ca atunci can termina recursia inca sa avem cuvantul dinainte)
                 copieCuvant = cuvant;
                 // Daca gasim ~ adica lambda atunci doar stergem litera neterminala din cuvantul curent
-                if(g.productii[i].cuvant == "~") {
+                if (g.productii[i].cuvant == "~") {
                     copieCuvant.erase(pozitieNeterminal, 1);
                 }
-                // Altfel, stergem litera si inlocuim cu stringul corespunzator productiei
+                    // Altfel, stergem litera si inlocuim cu stringul corespunzator productiei
                 else {
                     copieCuvant.erase(pozitieNeterminal, 1);
                     copieCuvant.insert(pozitieNeterminal, g.productii[i].cuvant);
@@ -85,23 +86,23 @@ int main() {
     }
 
     // Afisam gramatica
-    cout<<"Gramatica este: \n";
+    cout << "Gramatica este: \n";
     for (int i = 0; i < gramatica.nrProductii; i++) {
-        cout<<gramatica.productii[i].litera<<":"<<gramatica.productii[i].cuvant<<endl;
+        cout << gramatica.productii[i].litera << ":" << gramatica.productii[i].cuvant << endl;
     }
-    cout<<endl;
+    cout << endl;
 
     // Setam cuvantul de inceput sa fie 'S' adica sa inceapa din starea S generarea
     string stareInceput;
     stareInceput.push_back('S');
-    cout<<"Introduceti limita de litere pana la care se vor genera cuvintele gramaticii:";
-    cin>>limitaLitere;
+    cout << "Introduceti limita de litere pana la care se vor genera cuvintele gramaticii:";
+    cin >> limitaLitere;
 
     // Generam cuvintele pana in limita 'limitaLitere' si apoi le afisam
     generareCuvinte(stareInceput, gramatica, limitaLitere + 1);
-    cout<<"Cuvintele generate sunt: \n";
-    for(int i = gramatica.cuvinteGenerate.size() - 1; i >= 0; i--) {
-        cout<<gramatica.cuvinteGenerate[i]<<endl;
+    cout << "Cuvintele generate sunt: \n";
+    for (int i = gramatica.cuvinteGenerate.size() - 1; i >= 0; i--) {
+        cout << gramatica.cuvinteGenerate[i] << endl;
     }
 
 
